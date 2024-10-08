c204226d461ba09ee305ef6aef2f72ff3f182629
TitidTerbang <yudisbaek@gmail.com>
2024-10-08 16:07:37 +0700

    menghapus boilerplate, mengubah yaml,login_page ✓

--- README.md
+++ README.md
@@ -7,21 +7,8 @@ JIKA BAGIAN KALIAN UDAH SELESAI BANTU YANG LAIN TAPI TANYA DLU SAMA YANG PEGANG 
 flutter 4.6.5
 image_picker : ^0.8.7
 
-
-
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
-Projek pemrograman mobile kel 5
+## SEKEDAR TIPS CARA NGERUN FLUTTER
+1. Hapus direktori `android`
+2. Hapus direktori `build`
+3. Di Terminal, ketik `flutter create .` lalu jalankan
+4. Jalankan `flutter run` di terminal
--- lib/app/modules/home/views/login_page.dart
+++ lib/app/modules/home/views/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/user_model.dart';
 import '../controllers/login_controller.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final LoginController controller = LoginController();
@@ -12,46 +12,105 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  // <-- Widget Text untuk judul
+                  "KASIRKU",
+                  style: TextStyle(
+                    fontSize: 32, // Ubah ukuran font di sini
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 32),
+                // <-- Tambahkan jarak antara judul dan input
+                TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(21, 173, 180, 1),
+                    ),
+                    onPressed: () {
+                      User user = User(
+                        username: usernameController.text,
+                        password: passwordController.text,
+                      );
+                      controller.login(user, context);
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(21, 173, 180, 1),
+                      ),
+                      onPressed: () {
+                      },
+                      child: const Text(
+                        "Buat Akun",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(width: 19), // <-- Jarak antar tombol
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(21, 173, 180, 1),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Lupa Password",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                User user = User(
-                  username: usernameController.text,
-                  password: passwordController.text,
-                );
-                controller.login(user, context);
-              },
-              child: const Text('Login'),
-            )
-          ],
+          ),
         ),
       ),
     );
   }
-
 }
-
