f3b2fe328deb52bc093c4a6993f2cf1cdda196ab
TitidTerbang <yudisbaek@gmail.com>
2024-11-09 15:49:29 +0700

    implementasi fitur change profile picture dengan supabase, uid firebase sebagai patokan supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'routes/app_pages.dart';
 import 'routes/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
@@ -9,9 +10,16 @@ import 'firebase_options.dart'; // Import konfigurasi Firebase (dihasilkan oleh 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // Sesuaikan dengan konfigurasi Firebase kamu
+    options: DefaultFirebaseOptions
+        .currentPlatform,
   );
   runApp(const MyApp());
+
+  await Supabase.initialize(
+    url: 'https://wuujwzxuczpkvtnmkcys.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1dWp3enh1Y3pwa3Z0bm1rY3lzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTEzNjAzNCwiZXhwIjoyMDQ2NzEyMDM0fQ.q_LUQfmKSognvb7bui5i9OQfkz-Bvoclz6OlV3OmQ5c',
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,8 +32,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: AppRoutes.login, // Inisialisasi rute pertama
-      getPages: AppPages.pages, // Daftar rute
+      initialRoute: AppRoutes.login,
+      // Inisialisasi rute pertama
+      getPages: AppPages.pages,
+      // Daftar rute
       debugShowCheckedModeBanner: false,
     );
   }
